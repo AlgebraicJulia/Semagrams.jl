80e3068326361882171530f88b295ee3446b2a96
Owen Lynch <root@owenlynch.org>
2021-06-11 22:43:25 -0400

    OK but actually this time

--- docs/make.jl
+++ docs/make.jl
@@ -11,5 +11,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/AlgebraicJulia/Semagrams.jl.git"
+  repo = "github.com/AlgebraicJulia/Semagrams.jl.git",
+  devbranch = "main"
 )
