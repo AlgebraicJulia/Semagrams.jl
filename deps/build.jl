4d7be4c9c04050f9b281a944cee65118e992a6e9
Owen Lynch <root@owenlynch.org>
2021-07-12 15:36:57 -0700

    bundle handling: build from source in development mode

--- deps/build.jl
+++ deps/build.jl
@@ -10,7 +10,28 @@ const BUNDLE_URL = "https://unpkg.com/semagrams@$SEMAGRAMS_VERSION/dist/app.bund
 const BUNDLE_DIR = normpath(joinpath(@__DIR__, "bundles"))
 const BUNDLE_PATH = normpath(joinpath(BUNDLE_DIR, "app.bundle.js"))
 
-mkpath(BUNDLE_DIR)
+function download_js_bundle()
+  mkpath(BUNDLE_DIR)
 
-@info "Downloading Semagrams bundle from unpkg"
-download(BUNDLE_URL, BUNDLE_PATH)
+  @info "Downloading Semagrams bundle from unpkg"
+  download(BUNDLE_URL, BUNDLE_PATH)
+end
+
+function build_js_bundle()
+  mkpath(BUNDLE_DIR)
+
+  cd(joinpath(@__DIR__, "..", "javascript"))
+
+  @info "Building Semagrams bundle"
+  run(`npm run devbuild`)
+
+  run(`cp dist/app.bundle.js ../deps/bundles/`)
+
+  run(`rm -rf dist`)
+end
+
+if get(ENV, "SEMAGRAMS_DEV", "false") == "true"
+  build_js_bundle()
+else
+  download_js_bundle()
+end
