53e00873160f978430682337ddc3412b64c84acd
Owen Lynch <root@owenlynch.org>
2022-07-21 15:06:46 +0100

    ENH: basic box sprite

--- next/scala/src/main/scala/semagrams/Sprite.scala
+++ next/scala/src/main/scala/semagrams/Sprite.scala
@@ -5,27 +5,6 @@ import com.raquo.laminar.api.L.{*, given}
 import com.raquo.laminar.nodes.ReactiveSvgElement
 import semagrams.util.Complex
 
-/*
- * A sprite is a fairly complicated beast. Here are some properties
- *
- * - A sprite has reactive state. The sprite can both receive and send updates to its state.
- * - The state of a sprite is a map of properties
- * - A sprite has named anchors. These are positions that depend on the state of the sprite, that other sprites can attach to.
- * - The boundary of a sprite can be queried at any angle
- * - A sprite can have content that it resizes dynamically in response to
- * - Sprites can be rendered in dynamic svg, or TikZ
- * - A sprite can
- *
- * The data flow goes
- *
- * Controllers -> Sprites + PropMaps -> Events -> Controllers
- */
-
-/*
- * This should eventually represent a reference to an anchor of another sprite
- */
-abstract class ExternalAnchor
-
 abstract class AbstractProperty
 
 abstract class Property[String] extends AbstractProperty
@@ -39,12 +18,7 @@ case class MinimumHeight() extends Property[Double]
 case class Content() extends Property[String]
 case class Center() extends Property[Complex]
 
-// Sprites will use this property in different ways
-// For instance, an edge sprite might use Anchor(0) and Anchor(1) for
-// source and target
-case class Anchor(n: Int) extends Property[ExternalAnchor]
-
-case class PropMap(map : Map[AbstractProperty, Any]) {
+case class PropMap(map: Map[AbstractProperty, Any]) {
   def apply[T](p: Property[T]): T = {
     map(p).asInstanceOf[T]
   }
@@ -55,6 +29,10 @@ case class PropMap(map : Map[AbstractProperty, Any]) {
 
   def +[T](kv: (Property[T], T)): PropMap = {
     this.copy(map = map + kv)
+  }
+
+  def ++(other: PropMap): PropMap = {
+    this.copy(map = map ++ other.map)
   }
 }
 
@@ -67,26 +45,16 @@ object PropMap {
 trait Sprite {
   def present(
     ent: Entity,
-    initProps: PropMap,
+    init: PropMap,
     updates: Signal[PropMap],
   ): SvgElement
+
+  def boundaryPt(
+    data: PropMap,
+    dir: Double
+  ): Complex
 }
 
-// class SpriteCollection(
-//   val S: Sprite,
-//   val sprites: Signal[List[(Entity, S.Model)]]
-// ) extends Modifier[SvgElement] {
-//   def present(): Signal[List[SvgElement]] = {
-//     sprites.split(_._1)({ case (ent, init, updates) => S.present(ent, init._2, updates.map(_._2))})
-//   }
-
-//   override def apply(el: SvgElement) = el.amend(
-//     children <-- present()
-//   )
-// }
-
-// object SpriteCollection {
-//   def apply(S: Sprite, sprites: Signal[List[(Entity, S.Model)]]) = {
-//     new SpriteCollection(S, sprites)
-//   }
-// }
+/**
+ * Sprites should also possibly have default properties?
+ */
--- next/scala/src/main/scala/semagrams/sprites/Box.scala
+++ next/scala/src/main/scala/semagrams/sprites/Box.scala
@@ -3,117 +3,61 @@ package semagrams.sprites
 import com.raquo.laminar.api.L.svg._
 import com.raquo.laminar.api._
 import semagrams.util._
-import semagrams.util.CustomAttr._
-import semagrams.controllers._
 import semagrams._
+import semagrams.util.CustomAttr.CustomSvgAttr
+import semagrams.util.CustomAttr.SvgBinder
 
-case class BoxData(
-  dims: Complex,
-  pos: Complex
-)
+class XYAttr extends CustomSvgAttr[Complex] {
+  def applyAttrs(binder: SvgBinder[Complex]): Unit = {
+    binder(x, _.x.toString)
+    binder(y, _.y.toString)
+  }
+}
 
-object BoxData {
-  def fromProps(props: PropMap): BoxData = {
-    val center = props(Center())
-    val width = props(MinimumWidth())
-    val height = props(MinimumHeight())
+val xy = new XYAttr()
+
+class WHAttr extends CustomSvgAttr[Complex] {
+  def applyAttrs(binder: SvgBinder[Complex]): Unit = {
+    binder(width, _.x.toString)
+    binder(height, _.y.toString)
+  }
+}
+
+val wh = new WHAttr()
+
+extension[A,B] (s: L.Signal[Tuple2[A,B]])
+  def splitTuple: Tuple2[L.Signal[A], L.Signal[B]] = (s.map(_._1), s.map(_._2))
+
+case class Box() extends Sprite {
+  def geom(data: PropMap): (Complex, Complex) = {
+    val center = data(Center())
+    val width = data(MinimumWidth())
+    val height = data(MinimumHeight())
     val dims = Complex(width, height)
     val pos = center - dims / 2
-    BoxData(dims, pos)
+    (pos, dims)
+  }
+
+  def geomUpdater(updates: L.Signal[PropMap]) = {
+    val (pos, dims) = updates.map(geom).splitTuple
+    List(xy <-- pos, wh <-- dims)
+  }
+
+  def styleUpdater(data: L.Signal[PropMap]) = {
+    List(
+      fill <-- data.map(_(Fill())),
+      stroke <-- data.map(_(Stroke()))
+    )
+  }
+
+  def present(ent: Entity, init: PropMap, updates: L.Signal[PropMap]) = {
+    rect(
+      geomUpdater(updates),
+      styleUpdater(updates)
+    )
+  }
+
+  def boundaryPt(data: PropMap, dir: Double) = {
+    Complex(0,0)
   }
 }
-
-class BoxDataAttr extends CustomSvgAttr[BoxData] {
-  def applyAttrs(binder: SvgBinder[BoxData]): Unit = {
-    binder(width, _.dims.x.toString)
-    binder(height, _.dims.y.toString)
-    binder(x, _.pos.x.toString)
-    binder(y, _.pos.y.toString)
-  }
-}
-
-val boxData = new BoxDataAttr()
-
-enum BoxEvent {
-  case NewParams(ent: Entity, params: BoxData)
-}
-
-enum Side {
-  case Top
-  case Bottom
-  case Left
-  case Right
-
-  def sideBox(thickness: Double): BoxData => BoxData = {
-    this match {
-      case Top => { case BoxData(dims, pos) => BoxData(Complex(dims.x, thickness), Complex(pos.x, pos.y - thickness / 2)) }
-      case Bottom => { case BoxData(dims, pos) => BoxData(Complex(dims.x, thickness), Complex(pos.x, pos.y + dims.y - thickness / 2)) }
-      case Left => { case BoxData(dims, pos) => BoxData(Complex(thickness, dims.y), Complex(pos.x - thickness / 2, pos.y)) }
-      case Right => { case BoxData(dims, pos) => BoxData(Complex(thickness, dims.y), Complex(pos.x + dims.x - thickness / 2, pos.y)) }
-    }
-  }
-
-  def vertical: Boolean = {
-    this match {
-      case Top => true
-      case Bottom => true
-      case _ => false
-    }
-  }
-
-  def sidePos: BoxData => Complex = {
-    this match {
-      case Top => { case BoxData(dims, pos) => Complex(pos.x, pos.y) }
-      case Bottom => { case BoxData(dims, pos) => Complex(pos.x, pos.y + dims.y) }
-      case Left => { case BoxData(dims, pos) => Complex(pos.x, pos.y) }
-      case Right => { case BoxData(dims, pos) => Complex(pos.x + dims.x, pos.y) }
-    }
-  }
-
-  def resize: (Complex, BoxData) => BoxData = {
-    this match {
-      case Top => { case (newpos, BoxData(dims, pos)) => BoxData(Complex(dims.x, (pos.y - newpos.y) + dims.y), Complex(pos.x, newpos.y)) }
-      case Bottom => { case (newpos, BoxData(dims, pos)) => BoxData(Complex(dims.x, newpos.y - pos.y), Complex(pos.x, pos.y)) }
-      case Left => { case (newpos, BoxData(dims, pos)) => BoxData(Complex((pos.x - newpos.x) + dims.x, dims.y), Complex(newpos.x, pos.y)) }
-      case Right => { case (newpos, BoxData(dims, pos)) => BoxData(Complex(newpos.x - pos.x, dims.y), Complex(pos.x, pos.y)) }
-    }
-  }
-}
-
-// case class Box(
-//   hover: HoverController,
-//   drag: DragController,
-//   updater: L.Observer[BoxEvent]
-// ) extends Sprite {
-//   type Model = BoxData
-
-//   def makeSide(ent: Entity, model: L.Var[Model], side: Side) = {
-//     val resize = side.resize
-//     rect(
-//       boxData <-- model.signal.map(side.sideBox(10)),
-//       stroke := "none",
-//       fill := "#fff",
-//       fillOpacity := "0",
-//       style := "cursor:"+(if side.vertical then "ns-resize" else "ew-resize"),
-//       drag.draggable(model.signal.map(side.sidePos), updater.contramap((pos: Complex) => BoxEvent.NewParams(ent, resize(pos, model.now()))))
-//     )
-//   }
-
-//   def present(ent: Entity, initModel: Model, updates: L.Signal[Model]) = {
-//     val model = L.Var(initModel)
-//     g(
-//       rect(
-//         updates --> model.writer,
-//         boxData <-- updates,
-//         stroke := "black",
-//         fill <-- hover.switchState(ent, "lightgrey", "white"),
-//         drag.draggable(updates.map(_.pos), updater.contramap(pos => BoxEvent.NewParams(ent, model.now().copy(pos = pos)))),
-//         hover.hoverable(ent),
-//       ),
-//       makeSide(ent, model, Side.Top),
-//       makeSide(ent, model, Side.Bottom),
-//       makeSide(ent, model, Side.Left),
-//       makeSide(ent, model, Side.Right),
-//     )
-//   }
-// }
