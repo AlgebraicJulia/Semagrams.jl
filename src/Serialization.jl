5382c305d3141569a9394d9301133bea01402a32
Owen Lynch <root@owenlynch.org>
2022-10-11 18:52:03 -0700

    FIXUP: use columns.jl agnostic API for accessing schema

--- src/Serialization.jl
+++ src/Serialization.jl
@@ -8,25 +8,25 @@ function read_acset(name::String, ::Type{T}, json::JSON3.Object, deserializers::
   acs = T()
   s = acset_schema(acs)
   ob_maps = Dict{Symbol, Dict{Int, Int}}()
-  for ob in s.obs
+  for ob in ob(s)
     ob_maps[ob] = Dict{Int,Int}()
     for i in json.obs[ob]
       ob_maps[ob][i] = add_part!(acs, ob)
     end
   end
 
-  for f in s.homs
+  for f in hom(s)
     vs = Dict(map(Tuple, [json.homs[f]...]))
-    for i in json.obs[s.doms[f]]
-      acs[ob_maps[s.doms[f]][i],f] = ob_maps[s.codoms[f]][vs[i]]
+    for i in json.obs[dom(s,f)]
+      acs[ob_maps[dom(s,f)][i],f] = ob_maps[codom(s,f)][vs[i]]
     end
   end
 
-  for f in s.attrs
+  for f in attr(s)
     vs = Dict(map(Tuple, [json.attrs[f]...]))
-    for i in json.obs[s.doms[f]]
+    for i in json.obs[dom(s,f)]
       if i ∈ keys(vs)
-        acs[ob_maps[s.doms[f]][i],f] = deserializers[s.codoms[f]](vs[i])
+        acs[ob_maps[dom(s,f)][i],f] = deserializers[codom(s,f)](vs[i])
       end
     end
   end
